898414e74df66de54b7e2c8ab4d20d4a42067b2c
Linhtranne <147390264+Linhtranne@users.noreply.github.com>
2023-11-07 11:10:20 +0700

    Delete thuchanh2.3.cpp

--- thuchanh2.3.cpp
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main(){
-	double mark_html = 9.7;
-	double mark_css = 6.5;
-	double mark_javascript = 8.1;
-	double avg_mark = (mark_html + mark_css * 2 + mark_javascrpit * 3)/6
-}
